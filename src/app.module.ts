8cd42a130ed2f37ab87ab58ed660d53810e93cae
ilya-lukashenya <il.lukashenya@gmail.com>
2024-12-03 23:01:07 +0300

    Fix app imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
