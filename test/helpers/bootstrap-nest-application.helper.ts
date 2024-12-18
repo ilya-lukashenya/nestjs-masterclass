52bb5acf01fe7a111f2faf750ccd3a91ef32403c
ilya-lukashenya <il.lukashenya@gmail.com>
2024-12-18 10:19:02 +0300

    Add e2e tests for user module.

---
+++ test/helpers/bootstrap-nest-application.helper.ts
@@ -0,0 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from '../../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { appCreate } from '../../src/app.create';
+
+export async function bootstrapNestApplication(): Promise<INestApplication> {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule, ConfigModule],
+    providers: [ConfigService],
+  }).compile();
+
+  const app: INestApplication = moduleFixture.createNestApplication();
+  await appCreate(app);
+  await app.init();
+  return app;
+}
